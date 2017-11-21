0b794e00d491432623261513e9ee6d9418de0901
AlastairDanleyCox <alastair.danleycox@totaljobsgroup.com>
2017-11-21 07:42:39 +0000

    Added reminder for selenium tests at a later time Better named tests

--- manager.web.tests/Controllers/TeamControllerTest.cs
+++ manager.web.tests/Controllers/TeamControllerTest.cs
@@ -5,16 +5,23 @@ using NUnit.Framework;
 namespace manager.web.tests.Controllers
 {
     [TestFixture]
-    public class UnitTest1
+    public class TeamControllerTests
     {
         [Test]
-        public void TestMethod1()
+        public void Create_Team_Request_Should_Return_Create_Team_View()
         {
             var controller = new TeamController();
 
             ViewResult actualResult = controller.Create() as ViewResult;
 
             Assert.IsNotNull(actualResult);
+        }
+
+
+        [Ignore("Consider selenium tests")]
+        public void ViewTest()
+        {
+            Assert.AreEqual("Create", "ViewBag.Title");
         }
     }
 }
